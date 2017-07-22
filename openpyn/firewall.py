93bbee5d0b1d4f6b5a6b9c5a6e61511ba62f73f8
Joty <joty@mygnu.org>
2017-07-23 02:10:55 +0930

    Allowing custom ports on the local network (SSH,HTTP etc) #38

--- openpyn/firewall.py
+++ openpyn/firewall.py
@@ -8,16 +8,19 @@ def clear_fw_rules():
     print("Flushing iptables INPUT and OUTPUT chains AND Applying default Rules")
     subprocess.run(["sudo", "iptables", "-F", "OUTPUT"])
     # allow all outgoing traffic
-    subprocess.run("sudo iptables -P OUTPUT ACCEPT", shell=True)
+    subprocess.run("sudo iptables -P OUTPUT ACCEPT".split())
 
     subprocess.run(["sudo", "iptables", "-F", "INPUT"])
     subprocess.run(["sudo", "iptables", "-A", "INPUT", "-i", "lo", "-j", "ACCEPT"])
     subprocess.run(["sudo", "iptables", "-A", "OUTPUT", "-o", "lo", "-j", "ACCEPT"])
-    subprocess.run("sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT", shell=True)
+    subprocess.run(
+        "sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT".split())
+    # allow ICMP traffic
+    subprocess.run("sudo iptables -A INPUT -p icmp --icmp-type any -j ACCEPT".split())
     # best practice, stops spoofing
-    subprocess.run("sudo iptables -A INPUT -s 127.0.0.0/8 -j DROP", shell=True)
+    subprocess.run("sudo iptables -A INPUT -s 127.0.0.0/8 -j DROP".split())
     # drop anything else incoming
-    subprocess.run("sudo iptables -P INPUT DROP", shell=True)
+    subprocess.run("sudo iptables -P INPUT DROP".split())
     return
 
 
@@ -29,9 +32,11 @@ def apply_fw_rules(interfaces_details, vpn_server_ip):
     subprocess.run(["sudo", "iptables", "-F", "INPUT"])
 
     # Allow all traffic out over the vpn tunnel
-    subprocess.run("sudo iptables -A OUTPUT -o tun+ -j ACCEPT", shell=True)
+    subprocess.run("sudo iptables -A OUTPUT -o tun+ -j ACCEPT".split())
     # accept traffic that comes through tun that you connect to
-    subprocess.run("sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -i tun+ -j ACCEPT", shell=True)
+    subprocess.run(
+        "sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED\
+         -i tun+ -j ACCEPT".split())
     for interface in interfaces_details:
 
         # if interface is active with an IP in it, don't send DNS requests to it
@@ -45,11 +50,13 @@ def apply_fw_rules(interfaces_details, vpn_server_ip):
             if interface[0] != "lo":
                 # allow access to vpn_server_ip
                 subprocess.run(
-                    ["sudo", "iptables", "-A", "OUTPUT", "-o", interface[0], "-d", vpn_server_ip, "-j", "ACCEPT"])
+                    ["sudo", "iptables", "-A", "OUTPUT", "-o", interface[0],
+                        "-d", vpn_server_ip, "-j", "ACCEPT"])
                 # talk to the vpnServer ip to connect to it
                 subprocess.run(
                     ["sudo", "iptables", "-A", "INPUT", "-m", "conntrack",
-                        "--ctstate", "ESTABLISHED,RELATED", "-i", interface[0], "-s", vpn_server_ip, "-j", "ACCEPT"])
+                        "--ctstate", "ESTABLISHED,RELATED", "-i", interface[0],
+                        "-s", vpn_server_ip, "-j", "ACCEPT"])
 
                 # allow access to internal ip range
                 # print("internal ip with range", interface[2])
@@ -58,16 +65,28 @@ def apply_fw_rules(interfaces_details, vpn_server_ip):
                         interface[2], "-j", "ACCEPT"])
                 subprocess.run(
                     ["sudo", "iptables", "-A", "INPUT", "-m", "conntrack",
-                        "--ctstate", "ESTABLISHED,RELATED", "-i", interface[0], "-s", interface[2], "-j", "ACCEPT"])
+                        "--ctstate", "ESTABLISHED,RELATED", "-i", interface[0],
+                        "-s", interface[2], "-j", "ACCEPT"])
 
     # Allow loopback traffic
-    subprocess.run("sudo iptables -A INPUT -i lo -j ACCEPT", shell=True)
-    subprocess.run("sudo iptables -A OUTPUT -o lo -j ACCEPT", shell=True)
+    subprocess.run("sudo iptables -A INPUT -i lo -j ACCEPT".split())
+    subprocess.run("sudo iptables -A OUTPUT -o lo -j ACCEPT".split())
 
     # best practice, stops spoofing
-    subprocess.run("sudo iptables -A INPUT -s 127.0.0.0/8 -j DROP", shell=True)
+    subprocess.run("sudo iptables -A INPUT -s 127.0.0.0/8 -j DROP".split())
 
     # Default action if no other rules match
-    subprocess.run("sudo iptables -P OUTPUT DROP", shell=True)
-    subprocess.run("sudo iptables -P INPUT DROP", shell=True)
+    subprocess.run("sudo iptables -P OUTPUT DROP".split())
+    subprocess.run("sudo iptables -P INPUT DROP".split())
     return
+
+
+def internally_allow_ports(interfaces_details, internally_allowed):
+    for interface in interfaces_details:
+        # if interface is active with an IP in it, and not "tun*"
+        if len(interface) == 3 and "tun" not in interface[0]:
+            # Allow the specified ports on internal network
+            for port in internally_allowed:
+                subprocess.run(
+                    ("sudo iptables -A INPUT -p tcp --dport " + port + " -i " +
+                        interface[0] + " -s " + interface[2] + " -j ACCEPT").split())
