263b4e78c5c490e900ec50af0c88f14025d5b799
Joty <joty@mygnu.org>
2018-03-22 16:28:28 +1030

    Install files in the relative location, fix path for unzip

--- openpyn/__init__.py
+++ openpyn/__init__.py
@@ -2,16 +2,15 @@ import sys
 import os.path
 import subprocess
 
-__version__ = "2.4.2"
+__version__ = "2.4.5"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
-__basefilepath__ = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-__data_files__ = []     # will be overwritten for non Mac OS
+__data_files__ = []
+
+install_dir = os.path.dirname(os.path.abspath(__file__))
+# find "/", Excluding the very last one, +1 to keep '/' at the end
+__basefilepath__ = install_dir[:install_dir.rfind('/', 0, -1) + 1]
+print(__basefilepath__)
 
 if sys.platform == "linux":
     if subprocess.check_output(['/bin/uname', '-o']).decode(sys.stdout.encoding).strip() == "ASUSWRT-Merlin":
-        __basefilepath__ = "/opt/usr/share/openpyn/"
         __data_files__ = [('/opt/etc/init.d', ['./openpyn/S23openpyn'])]
-    else:
-        __data_files__ = [(__basefilepath__[:-1],
-                           ['./openpyn/scripts/manual-dns-patch.sh',
-                            './openpyn/scripts/update-resolv-conf.sh', './openpyn/config.json'])]
