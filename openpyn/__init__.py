ee31a111406a8be0f8495a38c4bd77f74a2752bf
Joty <joty@mygnu.org>
2018-03-25 12:15:49 +1030

    2.5.0.dev2

--- openpyn/__init__.py
+++ openpyn/__init__.py
@@ -2,14 +2,15 @@ import sys
 import os.path
 import subprocess
 
-__version__ = "2.5.0.dev1"
+__version__ = "2.5.0.dev2"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
 __data_files__ = []
 
 install_dir = os.path.dirname(os.path.abspath(__file__))
+print("install_dir", install_dir)
 # find "/", Excluding the very last one, +1 to keep '/' at the end
 __basefilepath__ = install_dir[:install_dir.rfind('/', 0, -1) + 1]
-
+print("__basefilepath__", __basefilepath__)
 if sys.platform == "linux":
     if subprocess.check_output(['/bin/uname', '-o']).decode(sys.stdout.encoding).strip() == "ASUSWRT-Merlin":
         __data_files__ = [('/opt/etc/init.d', ['./openpyn/S23openpyn'])]
