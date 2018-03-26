ea8d5378d99dcba49b8f81791a631f184aebcb7d
Joty <joty@mygnu.org>
2018-03-26 13:15:05 +1030

    dev5

--- openpyn/__init__.py
+++ openpyn/__init__.py
@@ -2,7 +2,7 @@ import sys
 import os.path
 import subprocess
 
-__version__ = "2.5.0.dev4"
+__version__ = "2.5.0.dev5"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
 __data_files__ = []
 
