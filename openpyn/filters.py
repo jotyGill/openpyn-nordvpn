37990f91f7f5339d3c8b01bc4697e2c82e9df6e2
1951FDG <1951FDG@users.noreply.github.com>
2018-05-27 02:25:55 +0200

    Bug fix, api name has changed to "Obfuscated Servers"

--- openpyn/filters.py
+++ openpyn/filters.py
@@ -63,7 +63,7 @@ def filter_by_type(json_response, p2p, dedicated, double_vpn, tor_over_vpn, anti
                 remaining_servers.append(eachServer)
             if double_vpn and ServerType["name"] == "Double VPN":
                 remaining_servers.append(eachServer)
-            if tor_over_vpn and ServerType["name"] == "Onion Over VPN":
+            if tor_over_vpn and ServerType["name"] == "Obfuscated Servers":
                 remaining_servers.append(eachServer)
             if anti_ddos and ServerType["name"] == "Anti DDoS":
                 remaining_servers.append(eachServer)
