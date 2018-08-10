73133f832bd4212299243bfab8c012e404aa74e7
Joty <joty@mygnu.org>
2018-08-10 11:50:54 +0930

    2.7.1.dev6

--- openpyn/__init__.py
+++ openpyn/__init__.py
@@ -4,7 +4,7 @@ import subprocess
 # import gc
 import verboselogs
 
-__version__ = "2.7.1.dev5"
+__version__ = "2.7.1.dev6"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
 __data_files__ = []
 __basefilepath__ = os.path.dirname(os.path.abspath(__file__)) + "/"
