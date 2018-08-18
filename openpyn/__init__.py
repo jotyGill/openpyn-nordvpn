aea1ea1d12719ef993ae26ffa74a38b0a692e65d
JGill <joty@mygnu.org>
2018-08-18 11:34:56 +0930

    keep current version

--- openpyn/__init__.py
+++ openpyn/__init__.py
@@ -2,7 +2,11 @@ import sys
 import os.path
 import subprocess
 
+<<<<<<< HEAD
 __version__ = "2.7.3.dev1"
+=======
+__version__ = "2.7.2"
+>>>>>>> master
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
 __data_files__ = []
 __basefilepath__ = os.path.dirname(os.path.abspath(__file__)) + "/"
