2c767c5e395af2ec6210b118592d744ec3af615d
JGill <joty@mygnu.org>
2019-09-09 23:39:03 +0930

    Raise error if not running_as_root

--- openpyn/credentials.py
+++ openpyn/credentials.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import verboselogs
-from openpyn import credentials_file_path
+from openpyn import credentials_file_path, root
 
 verboselogs.install()
 logger = logging.getLogger(__package__)
@@ -17,6 +17,10 @@ def check_credentials() -> bool:
 def save_credentials() -> None:
     if not sys.__stdin__.isatty():
         raise RuntimeError("Please run %s in interactive mode" % __name__)
+
+    if root.verify_running_as_root() is False:
+        raise RuntimeError("Please run as 'sudo openpyn --init' the first time. \
+Root access is needed to store credentials in '%s'." % credentials_file_path)
 
     logger.info("Storing credentials in '%s' with openvpn \
 compatible 'auth-user-pass' file format", credentials_file_path)
