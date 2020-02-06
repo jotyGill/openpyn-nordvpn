6266b7642e6fc1f000032cc65b3fa43cfc5bf9aa
JGill <joty@mygnu.org>
2020-02-06 16:21:03 +1030

    Properly implemented add_route()

--- openpyn/routes.py
+++ openpyn/routes.py
@@ -4,33 +4,51 @@ import sys
 
 # Add route to allow traffic to go out of the public ip
 # For opening services like ssh/http on anything on public net,
-# Needed for VPS.
-def add_route(interfaces_details: List) -> None:
-    count = 175
-    # print(interfaces_details)
-    # sys.exit()
-    for interface in interfaces_details:
-        # print(interface)
+# Needed on VPSs.
+def add_route():
+    table_number = "175"
 
-        # if interface is active with an IP in it, and not "tun*"
-        if len(interface) == 3 and "tun" not in interface[0]:
-            ip_addr = interface[2][:interface[2].find("/")]
-            count+=1
-            route_rule = "from {} lookup {}".format(ip_addr,count)
-            print(route_rule)
-            ip_rules = str(subprocess.check_output(["ip","rule","list"]))
-            if route_rule in ip_rules:
-                print("IP route rule already exists")
-                continue
-            subprocess.call([
-                "sudo", "ip",
-                "rule", "add",
-                "from", ip_addr,
-                "table", str(count),
-            ])
-            subprocess.call([
-                "sudo", "ip",
-                "route", "add",
-                "table", str(count),
-                "to", ip_addr,
-            ])
+    default_route = (
+        subprocess.check_output(["ip", "route", "show", "default"])
+        .decode(sys.stdout.encoding)
+        .strip()
+        .split()
+    )
+    default_gateway_ip = default_route[2]
+    # default_interface = default_route[4]
+
+    # Get the first ip addr reported by 'hostname --all-ip-addresses'
+    ip_addr = (
+        subprocess.check_output(["hostname", "--all-ip-addresses"])
+        .decode(sys.stdout.encoding)
+        .strip()
+        .split()[0]
+    )
+
+    route_rule = "from {} lookup {}".format(ip_addr, table_number)
+    # print(route_rule)
+    ip_rules = str(subprocess.check_output(["ip", "rule", "list"]))
+    if route_rule in ip_rules:
+        print("IP route rule already exists, skipping")
+    else:
+        # Add ip rule and route to send traffic through the default gateway.
+        subprocess.call(
+            ["sudo", "ip", "rule", "add", "from", ip_addr, "table", table_number]
+        )
+        # subprocess.call(
+        #     ["sudo", "ip", "route", "add", "table", table_number, "to", ip_addr + "/32", "dev", default_interface]
+        # )
+
+        subprocess.call(
+            [
+                "sudo",
+                "ip",
+                "route",
+                "add",
+                "table",
+                table_number,
+                "default",
+                "via",
+                default_gateway_ip,
+            ]
+        )
